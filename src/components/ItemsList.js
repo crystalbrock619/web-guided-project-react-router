d456bf18b0c4ea250704ba36043327daab228b5b
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-14 19:28:35 +0200

    wip

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -5,7 +5,8 @@ import { useRouteMatch } from 'react-router-dom'
 
 export default function ItemsList(props) {
   const { items } = props
-  const { url } = useRouteMatch()
+  const { url } = useRouteMatch() // returns an object with things, among them the current ulr
+  
 
   // We'll grab the current URL using the hook
 
@@ -17,7 +18,7 @@ export default function ItemsList(props) {
           key={item.id}
         >
           {/* 👉 STEP 6 - Link starts, navigates us from <current url> to <current url>/<id of the item> */}
-          <Link to={`/items-list/${item.id}`}>
+          <Link to={`${url}/${item.id}`}>
             <img
               className='items-list-image'
               src={item.imageUrl}
