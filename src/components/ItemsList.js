9a2c5204b63999ba837ea8dea3c762612db8f033
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-14 19:30:14 +0200

    wip

--- src/components/ItemsList.js
+++ src/components/ItemsList.js
@@ -5,11 +5,10 @@ import { useRouteMatch } from 'react-router-dom'
 
 export default function ItemsList(props) {
   const { items } = props
+  // We'll grab the current URL using the hook
   // returns an object with things, among them the current url
   // so we don't have to repeat it in our Links
   const { url, path } = useRouteMatch()
-
-  // We'll grab the current URL using the hook
 
   return (
     <div className='items-list-wrapper'>
