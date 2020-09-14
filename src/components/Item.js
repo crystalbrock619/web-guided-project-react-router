3b6361a6293dc7a6715c363a1d5b362921e53c93
Gabriel Cabrejas <gabriel@cabrejas.net>
2020-09-14 20:00:15 +0200

    wip

--- src/components/Item.js
+++ src/components/Item.js
@@ -12,8 +12,8 @@ export default function Item(props) {
   const { id } = useParams()
   const { url, path } = useRouteMatch() // as you know, url is actually the pathname piece of the URL
 
-  console.log('url', url)
-  console.log('path', path)
+  console.log('url', url) // makes Links easier
+  console.log('path', path) // makes Routes easier
 
   console.log('the MAGNIFICENT :id from the URL', id)
 
